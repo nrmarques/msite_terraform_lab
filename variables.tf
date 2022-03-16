dfc319916c17b73a7b1ab89b3170c039cdce34eb
nmarques <nuno.slmarques@email.com>
2022-03-16 09:02:51 +0000

    update

--- variables.tf
+++ variables.tf
@@ -4,17 +4,17 @@
 variable "mso_url" {
   type        = string
   description = "The URL of ACI MSO"
-  default = "https://10.150.178.65"
+  default = "MSO_URL"
 }
 
 variable "mso_username" {
   type        = string
   description = "Username of the MSO admin user"
-  default = "admin"
+  default = "user"
 }
 
 variable "mso_password" {
   type        = string
   description = "Password of the MSO admin user"
-  default = "cisco123"
+  default = "pass"
 }
